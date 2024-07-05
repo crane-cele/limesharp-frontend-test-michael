ab2a4e470ef910f79f7c5bdf636ff6fcdf23bf5c
crane <cele.name0719@gmail.com>
2024-07-05 09:55:29 +0200

    complete layout

---
+++ src/components/Grid.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="mx-auto">
+      <div class="flex flex-col items-center">
+        <h1 class="font-bold text-center">Experienced & Multi-Talented</h1>
+        <img src="../assets/underline.svg" />
+        <p class="text-[#65757E] text-center text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor leo, porttitor vel dictum at, aliquam et augue. Duis vel molestie risus, ut lobortis felis.
+        </p>
+
+      </div>
+    
+      <div class="grid items-start">
+        <GridItem 
+          v-for="(item, index) in items" 
+          :key="index" 
+          :description="item.description"
+          :isExpanded="index === expandedIndex"
+          @toggle-expand="toggleExpand(index)"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref } from 'vue';
+  import GridItem from './GridItem.vue';
+  
+  export default defineComponent({
+    name: 'Grid',
+    components: {
+      GridItem
+    },
+    setup() {
+      const items = ref([
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+        { description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vestibulum eros nisi, eu sagittis nisl feugiat quis.' },
+      ]);
+  
+      const expandedIndex = ref<number | null>(null);
+  
+      const toggleExpand = (index: number) => {
+        expandedIndex.value = expandedIndex.value === index ? null : index;
+      };
+  
+      return {
+        items,
+        expandedIndex,
+        toggleExpand
+      };
+    }
+  });
+  </script>
+  
---
+++ src/components/GridItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="rounded-lg overflow-hidden shadow-lg">
+      <div class="bg-[#57655f] h-24 flex">
+        <img src="../assets/mark.svg" width="22" alt="mark" />
+      </div>
+
+      <div class="flex flex-col gap-3 p-4 bg-blac">
+        <h2 class="text-xl font-bold text-center">BLOCK</h2>
+        <p class="m-auto text-center" :class="{ 'line-clamp-2': !isExpanded }">{{ description }}</p>
+        <button @click="$emit('toggle-expand')" class="flex items-center justify-center">
+          <img v-if="!isExpanded" src="../assets/plus.png" />
+          <img v-else src="../assets/minus.svg" />
+        </button>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'GridItem',
+    props: {
+      description: {
+        type: String,
+        required: true
+      },
+      isExpanded: {
+        type: Boolean,
+        required: true
+      }
+    }
+  });
+  </script>
+  
+  <style scoped>
+  .line-clamp-2 {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;  
+    overflow: hidden;
+  }
+  </style>
+  
